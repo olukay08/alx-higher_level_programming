6f152d1c286440d3fd41a7d36602049507391e27
oluwole2044 <olukay08@gmail.com>
2022-11-30 14:16:49 -0800

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+
+    if number < 0:
+
+        lastdig = (number * -1) % 10
+
+    else:
+
+        lastdig = number % 10
+
+    print("{}".format(lastdig), end="")
+
+    return lastdig
