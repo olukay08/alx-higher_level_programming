1f6fc75c306ca8e613d5513960b76540af414ed4
oluwole2044 <olukay08@gmail.com>
2022-11-30 14:08:59 -0800

    convert to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+
+    strup = ""
+
+    for i in range(len(str)):
+
+        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
+
+            strup = ord(str[i]) - 32
+
+        else:
+
+            strup = ord(str[i])
+
+        print("{:c}".format(strup), end="")
+
+    print()
